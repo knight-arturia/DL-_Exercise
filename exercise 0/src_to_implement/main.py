d1c24c2fdc95126d5d79ff6b929e287b5aded761
豪 付 <fuhao_dsst@163.com>
2020-11-14 04:47:16 +0800

    __init__ of ImageGenerator

--- exercise 0/src_to_implement/main.py
+++ exercise 0/src_to_implement/main.py
@@ -4,16 +4,26 @@ import matplotlib.pyplot as plt
 from pattern import Checker
 from pattern import Circle
 from pattern import Spectrum
-'''
-Checkerboard = Checker(600,60)
-Checkerboard.show()
+from pattern import ImageGenerator
 
-Circlegraph = Circle(600, 100, (300,200))
-Circlegraph.show()
 
-RGB_spec = Spectrum(600)
-RGB_spec.show()
-'''
+#Checker
+#Checkerboard = Checker(600,60)
+#Checkerboard.show()
 
-Circlegraph = Circle(1024, 200, (512, 256))
-Circlegraph.show()
+
+#Circle
+#Circlegraph = Circle(600, 100, (300,200))
+#Circlegraph.show()
+
+#Spectrum
+#RGB_spec = Spectrum(600)
+#RGB_spec.show()
+label_path = './Labels.json'
+file_path = './exercise_data/'
+gen = ImageGenerator(file_path,label_path,12, [32, 32, 3],rotation=False,mirroring=False,shuffle=False)
+
+
+
+                
+        
