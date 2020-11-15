540952bffc8063c3a0d2b3bf040b5af356f61814
unknown <knight.arturia@gmail.com>
2020-11-15 14:06:47 +0100

    generator test

--- exercise 0/src_to_implement/generator.py
+++ exercise 0/src_to_implement/generator.py
@@ -7,28 +7,60 @@ import matplotlib.pyplot as plt
 # In this exercise task you will implement an image generator. Generator objects in python are defined as having a next function.
 # This next function returns the next generated object. In our case it returns the input of a neural network each time it gets called.
 # This input consists of a batch of images and its corresponding labels.
-class ImageGenerator:
-    def __init__(self, file_path, label_path, batch_size, image_size, rotation=False, mirroring=False, shuffle=False):
-        # Define all members of your generator class object as global members here.
-        # These need to include:
-        # the batch size
-        # the image size
-        # flags for different augmentations and whether the data should be shuffled for each epoch
-        # Also depending on the size of your data-set you can consider loading all images into memory here already.
-        # The labels are stored in json format and can be directly loaded as dictionary.
-        # Note that the file names correspond to the dicts of the label dictionary.
 
+class ImageGenerator:
+    """
+    Define all members of your generator class object as global members here.
+    These need to include:
+    the batch size
+    the image size
+    flags for different augmentations and whether the data should be shuffled for each epoch
+    Also depending on the size of your data-set you can consider loading all images into memory here already.
+    The labels are stored in json format and can be directly loaded as dictionary.
+    Note that the file names correspond to the dicts of the label dictionary.
+    """
+    def __init__(self, file_path, label_path, batch_size, image_size, rotation=False, mirroring=False, shuffle=False):
+        self.file_p = file_path
+        self.json_p = label_path
+        self.batch_s = batch_size
+        self.image_s = image_size
+        self.rot = rotation
+        self.mir = mirroring
+        self.shu = shuffle 
         self.class_dict = {0: 'airplane', 1: 'automobile', 2: 'bird', 3: 'cat', 4: 'deer', 5: 'dog', 6: 'frog',
                            7: 'horse', 8: 'ship', 9: 'truck'}
         #TODO: implement constructor
 
+    """
+    This function creates a batch of images and corresponding labels and returns them.
+    In this context a "batch" of images just means a bunch, say 10 images that are forwarded at once.
+    Note that your amount of total data might not be divisible without remainder with the batch_size.
+    Think about how to handle such cases
+    """
     def next(self):
-        # This function creates a batch of images and corresponding labels and returns them.
-        # In this context a "batch" of images just means a bunch, say 10 images that are forwarded at once.
-        # Note that your amount of total data might not be divisible without remainder with the batch_size.
-        # Think about how to handle such cases
+        # read the labels from json file
+        with open(self.json_p) as labels_f:
+            load_label = json.load(labels_f)
+        
+        batch_list = []
+        image_list = []
+        # read the images from exercise_data
+        for i in range(9):
+            for file in os.listdir(self.file_p):
+                file_name = os.path.basename(file)
+                for label_key in load_label:
+                    if file_name == label_key:
+                        if label_key.value() == i:
+                            image_list.append(file_name)
+                        else:
+                            continue
+                    else:
+                        continue
+            batch =(image_list, i)
+            batch_list.append(batch)
+        
+        return batch_list
         #TODO: implement next method
-
         return images, labels
 
     def augment(self,img):
