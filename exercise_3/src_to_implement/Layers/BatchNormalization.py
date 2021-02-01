564e5a83792f15e78b5e2c71f085a15f339a3e3b
knight-arturia <knight.arturia@gmail.com>
2021-02-01 12:11:46 +0100

    exercise 4 begin

--- exercise_3/src_to_implement/Layers/BatchNormalization.py
+++ exercise_3/src_to_implement/Layers/BatchNormalization.py
@@ -3,187 +3,120 @@ import os
 import sys
 folder_path, _ = os.path.split(__file__)
 sys.path.append(folder_path)
-
 import numpy as np
 import Helpers
 
 class BatchNormalization(BaseLayer):
-
-    running_mean = None    
+    running_mean = None
     running_var = None
-    
     optimizer = None
-
     _image_tensor_shape = None
-
     gradient_bias = None
     gradient_weights = None
-
     flag4debug = 0
     
     def __init__(self, channel, momentum=0.8):
-
         super(BatchNormalization, self).__init__()
-        
         self.channel = channel
-
         self.bias, self.weights = self.initialize(channel)
-
-        self.momentum = momentum 
-        
-
-
-    def initialize(self,channel):
-                
-        bias = np.zeros(channel) #PB (channel,1)? channel?
-        
-        weights = np.ones(channel)
-
-        return bias,weights
-        
-
-    def reformat(self,tensor):
-
-        if tensor.ndim == 4:
-
-            self._image_tensor_shape = tensor.shape
-
-            return self.image2vec(tensor)
-
-        else:
-            
-            return self.vec2image(self._image_tensor_shape, tensor)
-
+        self.momentum = momentum
     
-    def image2vec(self,tensor):
-        
+    def initialize(self, channel):
+        bias = np.zeros(channel)  # PB (channel,1)? channel?
+        weights = np.ones(channel)
+        return bias, weights
+    
+    def reformat(self, tensor):
+        if tensor.ndim == 4:
+            self._image_tensor_shape = tensor.shape
+            return self.image2vec(tensor)
+        else:
+            return self.vec2image(self._image_tensor_shape, tensor)
+    
+    def image2vec(self, tensor):
         B, C, M, N = tensor.shape
-
         tensor = tensor.reshape((B, C, M * N))
-                
         tensor = tensor.transpose((0, 2, 1))
-
         tensor = tensor.reshape((B * M * N, C))
-        
         return tensor
-
-
-    def vec2image(self,shape, tensor):
-        
+    
+    def vec2image(self, shape, tensor):
         B, C, M, N = shape
-        
-        tensor = tensor.reshape((B, M * N, C))        
-        tensor = tensor.transpose((0, 2, 1))        
-        tensor = tensor.reshape((B, C, M, N))        
-
+        tensor = tensor.reshape((B, M * N, C))
+        tensor = tensor.transpose((0, 2, 1))
+        tensor = tensor.reshape((B, C, M, N))
         return tensor
-
-
+    
     def forward(self, input_tensor):
-        
-
         tensor_dimension = input_tensor.ndim
-        
-
         if tensor_dimension == 4:
-                        
             input_tensor = self.reformat(input_tensor)
-        
         self.input_tensor = input_tensor
-        
         # BMN = input_tensor.shape[0]
-
         '''
         if self.weights.shape[0] == 1:
-            
             self.weights = self.weights.repeat(BMN, axis=0)
             self.bias = self.bias.repeat(BMN, axis=0)
         '''
-        
-        if not(self.testing_phase):            
-            
-            mu = np.mean(input_tensor, axis=0)
-            #mu = np.expand_dims(mu, 0).repeat(BMN, axis=0)
-
-            var = np.var(input_tensor, axis=0)
-            #var = np.expand_dims(var, 0).repeat(BMN, axis=0)            
-
+        if not (self.testing_phase):
+            self.mu = np.mean(input_tensor, axis=0)
+            # mu = np.expand_dims(mu, 0).repeat(BMN, axis=0)
+            self.var = np.var(input_tensor, axis=0)
+            # var = np.expand_dims(var, 0).repeat(BMN, axis=0)
             # self.var = var
-        
-            
-            # calculate mean        
+            # calculate mean
             if self.running_mean is None:
-
-                self.running_mean = mu
-                self.running_var = var
-
+                self.running_mean = self.mu
+                self.running_var = self.var
             else:
-                self.running_mean = self.momentum * self.running_mean + (1-self.momentum) * mu # default momentum = 0.8
-                self.running_var = self.momentum * self.running_var + (1-self.momentum) * var   
-
-        xc = input_tensor - self.running_mean
-        xn = np.true_divide(xc, np.sqrt(self.running_var + np.finfo(np.float32).eps))
-        
+                self.running_mean = self.momentum * self.running_mean + (
+                            1 - self.momentum) * self.mu  # default momentum = 0.8
+                self.running_var = self.momentum * self.running_var + (1 - self.momentum) * self.var
+        if not self.testing_phase:
+            xc = input_tensor - self.mu
+            xn = np.divide(xc, np.sqrt(self.var + np.finfo(float).eps))
+        else:
+            xc = input_tensor - self.running_mean
+            xn = np.divide(xc, np.sqrt(self.running_var + np.finfo(float).eps))
         '''
         if np.sum(xc) > 0.0001:
             print("xc2")
             print(np.sum(xc))   
         '''
-
         self.xn = xn
-            
-
         output_tensor = np.multiply(xn, self.weights) + self.bias
-
         if tensor_dimension == 4:
-                        
             output_tensor = self.reformat(output_tensor)
-
         return output_tensor
-
+    
     def backward(self, error_tensor):
-        
         tensor_dimension = error_tensor.ndim
-        
         if tensor_dimension == 4:
-
             error_tensor = self.reformat(error_tensor)
-
         # BMN = error_tensor.shape[0]
-        
         # Gradient with respect to weights
-        #print(error_tensor.shape)
+        # print(error_tensor.shape)
         gradient_weights = np.multiply(error_tensor, self.xn)
         gradient_weights = np.sum(gradient_weights, axis=0)
-        #print(gradient_weights.shape)
+        # print(gradient_weights.shape)
         # gradient_weights = np.sum(gradient_weights, axis=0)
-        #gradient_weights = np.expand_dims(gradient_weights, 0).repeat(BMN, axis=0)
-        #print(gradient_weights.shape)
-        #print("vv\n")
+        # gradient_weights = np.expand_dims(gradient_weights, 0).repeat(BMN, axis=0)
+        # print(gradient_weights.shape)
+        # print("vv\n")
         self.gradient_weights = gradient_weights
-                
         # Gradient with respect to bias
-        gradient_bias = np.sum(error_tensor, axis=0)  #np.sum(error_tensor, axis=0)
-        #gradient_bias = np.expand_dims(gradient_bias, 0).repeat(BMN, axis=0)
-
+        gradient_bias = np.sum(error_tensor, axis=0)  # np.sum(error_tensor, axis=0)
+        # gradient_bias = np.expand_dims(gradient_bias, 0).repeat(BMN, axis=0)
         self.gradient_bias = gradient_bias
-
-
         # Gradient with respect to input
-        gradient_input = Helpers.compute_bn_gradients(error_tensor, self.input_tensor, self.weights, self.running_mean, self.running_var)
-        #def compute_bn_gradients(error_tensor, input_tensor, weights, mean, var, eps=np.finfo(float).eps):
-            # computation of the gradient w.r.t the input for the batch_normalization layer
+        gradient_input = Helpers.compute_bn_gradients(error_tensor, self.input_tensor, self.weights, self.mu, self.var, eps = np.finfo(float).eps)
+        # def compute_bn_gradients(error_tensor, input_tensor, weights, mean, var, eps=np.finfo(float).eps):
+        # computation of the gradient w.r.t the input for the batch_normalization layer
         # update weights and bias when optimiezer is defined
         if self.optimizer != None:
-
             self.weights = self.optimizer.calculate_update(self.weights, self.gradient_weights)
             self.bias = self.optimizer.calculate_update(self.bias, self.gradient_bias)
-        
         if tensor_dimension == 4:
-
             gradient_input = self.reformat(gradient_input)
         
         return gradient_input
-
-        
-
--- exercise_3/src_to_implement/Layers/RNN.py
+++ exercise_3/src_to_implement/Layers/RNN.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.core.fromnumeric import shape
-
+import copy
 class RNN:
     _optimizer = None
     '''
@@ -22,9 +22,9 @@ class RNN:
         self.W_y = np.random.uniform(0, 1, size=(hidden_size, output_size))
         self.B_y = np.random.uniform(0, 1, size=output_size)
         # W_hx is a combination of W_xh and W_hh
-        self.W_hx = np.random.uniform(0, 1, size=(input_size+hidden_size, hidden_size))
+        self.W_hx = np.random.uniform(0, 1, size=(input_size + hidden_size, hidden_size))
         self.B_hx = np.random.uniform(0, 1, size=hidden_size)
-    
+        self.gradient_W_hx = None
     '''
     to initialize the W_hx and bias
     '''
@@ -35,7 +35,6 @@ class RNN:
         self.B_hx = bias_initializer.initialize(self.B_hx.shape, self.W_hx.shape[0], self.hidden_size)
         self.W_y = weight_initializer.initialize(self.W_y.shape, self.hidden_size, self.output_size)
         self.B_y = bias_initializer.initialize(self.B_y.shape, self.hidden_size, self.output_size)
-    
     '''
     memorize is how many steps we need to memorize, k2
     '''
@@ -46,24 +45,28 @@ class RNN:
     def memorize(self, value):
         self.seq_flag = value
     
+    
     def forward(self, input_tensor):
-
+        
         output_tensor = np.zeros((input_tensor.shape[0], self.output_size))
         ht_tensor = np.zeros((input_tensor.shape[0], self.hidden_size))
+        
+        if self.seq_flag == False:
+            self.hidden_state = np.zeros(self.hidden_size)
+        
         for i in range(input_tensor.shape[0]):
-            # if sequence flag is false means a new start of back propagation sequence 
-            if self.seq_flag == False:
-                self.hidden_state = np.zeros(self.hidden_size)
+            # if sequence flag is false means a new start of back propagation sequence
                 # set the h_t-1 back to 0
                 # ht_tensor[i-1] = 0
-                self.seq_flag = True
-            
             # attach input vector with hidden state
+            
             state_plus_input = np.hstack((self.hidden_state, input_tensor[i]))
             ht = np.tanh(np.dot(state_plus_input, self.W_hx) + self.B_hx)
+            
             # save h_t for backward propagation
             ht_tensor[i] = ht
             self.hidden_state = ht
+            
             y1 = np.dot(self.hidden_state, self.W_y) + self.B_y
             output_tensor[i] = 1 / (1 + np.exp(-y1))
         
@@ -71,9 +74,7 @@ class RNN:
         self.input_tensor = input_tensor
         self.ht_tensor = ht_tensor
         self.output_tensor = output_tensor
-
         return output_tensor
-    
     '''
     give the optimizer for W_hx and bias
     '''
@@ -83,24 +84,26 @@ class RNN:
     @optimizer.setter
     def optimizer(self, Obj):
         self._optimizer = Obj
-    
+        self._optimizer1 = copy.deepcopy(Obj)
+        self._optimizer2 = copy.deepcopy(Obj)
+        self._optimizer3 = copy.deepcopy(Obj)
+
     '''
     cal all regularization loss
     '''
     @property
     def calculate_regularization_loss(self):
-        return (self.optimizer.reg.norm(self.W_hx) 
-            + self.optimizer.reg.norm(self.W_y))
-
+        return (self.optimizer.reg.norm(self.W_hx)
+                + self.optimizer.reg.norm(self.W_y))
     '''
-    access to W_hx
+    access to W_hx and gradient_W_hx
     '''
     @property
     def gradient_weights(self):
-        return self.W_hx
+        return self.gradient_W_hx
     @gradient_weights.setter
     def gradient_weights(self, Matrix):
-        self.W_hx = Matrix
+        self.gradient_W_hx = Matrix
     
     @property
     def weights(self):
@@ -108,15 +111,11 @@ class RNN:
     @weights.setter
     def weights(self, Matrix):
         self.W_hx = Matrix
-    
-    
     '''
     the time steps of iteration is given by k2
     '''
     def backward(self, error_tensor):
-        
         # print('error_shape = ', error_tensor.shape)
-        
         # init all gradient
         gradient_W_y = np.zeros(self.W_y.shape)
         gradient_B_y = np.zeros(self.B_y.shape)
@@ -124,46 +123,62 @@ class RNN:
         # gradient W_hx is the combine of W_hh and W_xh
         gradient_W_hx = np.zeros(self.W_hx.shape)
         gradient_B_hx = np.zeros(self.B_hx.shape)
+        
         # gradient_W_xh = np.zeros((self.input_size, self.hidden_size))
         # gradient_W_hh = np.zeros((self.hidden_size, self.hidden_size))
-        d_B_hx = np.zeros(self.B_hx.shape)
-        
+        # d_B_hx = np.zeros(self.B_hx.shape)
         output_error_tensor = np.zeros(self.input_tensor.shape)
+        
+        e_h = 0
         
         # a inverse iteration from back to forward in sequence
         for i in np.arange(error_tensor.shape[0])[::-1]:
             
             d_sigmoid = self.output_tensor[i] * (1 - self.output_tensor[i])
-            e_h2 = np.dot(error_tensor[i] * d_sigmoid,  self.W_y.T)
-
-            gradient_W_y = np.outer(self.ht_tensor[i], error_tensor[i] * d_sigmoid)
-            gradient_B_y = error_tensor[i] * d_sigmoid
-
+            
+            e_h2 = np.dot(error_tensor[i] * d_sigmoid, self.W_y.T) + e_h
+            
+            gradient_W_y += np.outer(self.ht_tensor[i], error_tensor[i] * d_sigmoid)
+            gradient_B_y += error_tensor[i] * d_sigmoid
+            
             d_tanh = 1 - self.ht_tensor[i] ** 2
+            
             e_h1 = e_h2 * d_tanh
-
             d_W_xh = self.input_tensor[i].reshape((-1, 1))
-            d_W_hh = self.ht_tensor[i-1].reshape((-1, 1))
-            gradient_W_xh = np.outer(d_W_xh.T, e_h1)
-            gradient_W_hh = np.outer(d_W_hh.T, e_h1)
-            gradient_W_hx = np.vstack((gradient_W_hh, gradient_W_xh))
+            
+            if i == 0:
+                d_W_hh = np.zeros_like(d_W_hh)
+            else:
+                d_W_hh = self.ht_tensor[i - 1].reshape((-1, 1))
+            
+            gradient_W_hx +=  np.vstack([d_W_hh, d_W_xh]) @ e_h1[:, None].T
             # print("gradient_W_xh shape: ", gradient_W_xh.shape)
             # print("gradient_W_hh shape: ", gradient_W_hh.shape)
+            gradient_B_hx += e_h1
+            W_xh = self.W_hx
 
-            gradient_B_hx = e_h1
+            e_x = np.dot(e_h1, W_xh.T)[self.hidden_size:]
+            e_h = np.dot(e_h1, W_xh.T)[:self.hidden_size]
 
-            W_xh = self.W_hx[self.hidden_size : self.hidden_size+self.input_size] 
-            e_x = np.dot(e_h1, W_xh.T)
             output_error_tensor[i] = e_x
-
+        
+        self.gradient_W_hx = gradient_W_hx
         # update the W_hx and bias
+        
         if self._optimizer:
             self.W_y = self._optimizer.calculate_update(self.W_y, gradient_W_y)
-            self.B_y = self._optimizer.calculate_update(self.B_y, gradient_B_y)
-            self.W_hx = self._optimizer.calculate_update(self.W_hx, gradient_W_hx)
-            self.B_hx = self._optimizer.calculate_update(self.B_hx, gradient_B_hx)
+            self.B_y = self._optimizer1.calculate_update(self.B_y, gradient_B_y)
+            self.W_hx = self._optimizer2.calculate_update(self.W_hx, gradient_W_hx)
+            self.B_hx = self._optimizer3.calculate_update(self.B_hx, gradient_B_hx)
         
         return output_error_tensor
+
+
+
+
+
+
+
         '''
             # ready for the back propagation to k2 steps
             # cut W_hx and get a W_hh, for gradient d_ht  
